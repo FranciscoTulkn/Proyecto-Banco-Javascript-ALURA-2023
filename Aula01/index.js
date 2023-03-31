56daef311065375f53a0d4fa91dab2a7c874f87a
Francisco Javier Tulcán Rodríguez <tufran13@gmail.com>
2023-03-30 23:18:55 -0500

    Clase numoer 1 terminada

--- Aula01/index.js
+++ Aula01/index.js
@@ -9,41 +9,56 @@ class CuentaCorriente{
   agencia;
 
   // metodos
+  // Metodo para inicializar los valores de los atributos de la cuenta corriente
+  constructor (){
+    this.numero = "";
+    this.saldo = 0;
+    this.agencia = "";
+  }
+
+  // Metodo para el deposito en la cuenta
   depositoEnCuenta(valor){
     this.saldo += valor;
   }
+
+  // Metodo para el retiro en la cuenta
+  retiroEnCuenta(valor){
+    this.saldo -= valor;
+  }
 }
-// Constructor de cliente1
-const Cliente1 = new Cliente();
-// atributos de la clase cliente
-Cliente1.nombreCliente = "Leonardo";
-Cliente1.dniCliente = "27487092";
-// Constructor cuentacorriente1
-const cuentaCorriente1 = new CuentaCorriente();
+// // Constructor de cliente1
+// const Cliente1 = new Cliente();
+// // atributos de la clase cliente
+// Cliente1.nombreCliente = "Leonardo";
+// Cliente1.dniCliente = "27487092";
+// // Constructor cuentacorriente1
+// const cuentaCorriente1 = new CuentaCorriente();
 
-cuentaCorriente1.numero = "12345678";
-cuentaCorriente1.saldo = 2000;
+// cuentaCorriente1.numero = "12345678";
+// cuentaCorriente1.saldo = 2000;
 
-// Cosntructor cliente2
-const Cliente2 = new Cliente();
+// // Cosntructor cliente2
+// const Cliente2 = new Cliente();
 
-Cliente2.nombreCliente = "Maria";
-Cliente2.dniCliente = "67190153";
+// Cliente2.nombreCliente = "Maria";
+// Cliente2.dniCliente = "67190153";
 
-// Constructor cuentacorriente2
-const cuentaCorriente2 = new CuentaCorriente();
+// // Constructor cuentacorriente2
+// const cuentaCorriente2 = new CuentaCorriente();
 
-cuentaCorriente2.numero = "98754";
-cuentaCorriente2.saldo = 8000;
+// cuentaCorriente2.numero = "98754";
+// cuentaCorriente2.saldo = 8000;
 
-// const Cliente3 = new Cliente();
 
-// Cliente3.nombreCliente = "Juan";
-// Cliente3.dniCliente = "87082671";
-// Cliente3.numeroCuenta = "432876";
-// Cliente3.saldoCuenta = 5000;
+// console.log(Cliente1);
+// console.log(cuentaCorriente1);
+// console.log(Cliente2);
+// console.log(cuentaCorriente2);
 
-console.log(Cliente1);
-console.log(cuentaCorriente1);
-console.log(Cliente2);
-console.log(cuentaCorriente2);
+/* Ejemplos para crear isntancias de las propiedades de la clase Cuenta corriente*/
+
+const cuentaDeLuis = new CuentaCorriente();
+cuentaDeLuis.depositoEnCuenta(800);
+console.log(cuentaDeLuis.saldo);
+cuentaDeLuis.retiroEnCuenta(200);
+console.log(cuentaDeLuis.saldo);
