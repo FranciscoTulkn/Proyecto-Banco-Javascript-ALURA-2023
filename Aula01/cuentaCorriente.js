39a5fa224dcc16a299f959bacddf7c6786222fac
Francisco Javier Tulcán Rodríguez <tufran13@gmail.com>
2023-04-21 23:05:26 -0500

    Ajuste del codigo

--- Aula01/cuentaCorriente.js
+++ Aula01/cuentaCorriente.js
@@ -1,25 +1,79 @@
-export class CuentaCorriente{
-  numero;
-  #saldo; //Atributo con caracteristica de variable privada.
-  agencia;
+// export class CuentaCorriente{
 
-  // metodos
-  // Metodo para inicializar los valores de los atributos de la cuenta corriente
-  constructor (){
-    this.numero = "";
-    this.#saldo = 0;
-    this.agencia = "";
-  }
+//   #cliente;
+//   numero;
+//   #saldo; //Atributo con caracteristica de variable privada.
+//   agencia;
 
-  // Metodo para el deposito en la cuenta
-  depositoEnCuenta(valor){
-    if (valor > 0);
-      this.#saldo += valor;
-  }
+//   // metodos
+//   // Metodo para inicializar los valores de los atributos de la cuenta corriente
+//   constructor (){
 
-  // Metodo para el retiro en la cuenta
-  retiroEnCuenta(valor){
-    if (valor <= this.#saldo);
-      this.#saldo -= valor;
-  }
+//     this.#cliente = null;
+//     this.numero = "";
+//     this.#saldo = 0;
+//     this.agencia = "";
+//   }
+
+//   // Metodo para el deposito en la cuenta
+//   depositoEnCuenta(valor){
+//     if (valor > 0);
+//       this.#saldo += valor;
+//   }
+
+//   // Metodo para el retiro en la cuenta
+//   retiroEnCuenta(valor){
+//     if (valor <= this.#saldo);
+//       this.#saldo -= valor;
+//   }
+// }
+
+// Nuevo código
+import { Cliente } from "./cliente.js";
+
+export class CuentaCorriente
+{
+    #cliente;
+    numero;
+    agencia;
+    #saldo;
+
+    set setCliente(valor) {
+        if (valor instanceof Cliente)
+            this.#cliente = valor;
+    }
+
+    get getCliente() {
+        return this.#cliente;
+    }
+
+    constructor() {
+        this.#cliente = null;
+        this.numero = '';
+        this.agencia = '';
+        this.#saldo = 0;
+    }
+
+    depositoEnCuenta(valor) {
+        if (valor > 0)
+            this.#saldo += valor;
+        return this.#saldo;
+    }
+
+    retirarDeCuenta(valor) {
+        if (valor <= this.#saldo)
+            this.#saldo -= valor;
+        return this.#saldo;
+    }
+
+    verSaldo() {
+        return this.#saldo;
+    }
+
+    transferirParaCuenta(valor,cuentaDestino) {
+        this.retirarDeCuenta(valor);
+        cuentaDestino.depositoEnCuenta(valor);
+        valor = 200;
+        valor = valor*1000;
+    }
 }
--- Aula01/index.js
+++ Aula01/index.js
@@ -1,18 +1,76 @@
-// Importanto las clases
-import { Cliente } from "./cliente.js";
-import { CuentaCorriente } from "./cuentaCorriente.js";
+// // Importanto las clases
+// import { CuentaCorriente } from "./Aula01/cuentaCorriente.js";
+// import {Cliente} from './Aula01/cliente.js';
 
-const cuentaDeLuis = new CuentaCorriente();
-let saldo = cuentaDeLuis.verSaldo();
-console.log("El saldo actual es" + saldo);
+// const cuentaDeLuis = new CuentaCorriente();
+// let saldo = cuentaDeLuis.verSaldo();
+// console.log("El saldo actual es" + saldo);
 
-/* Ejemplos para crear isntancias de las propiedades de la clase Cuenta corriente*/
-// Deposito a la cuenta corrienta
-saldo = cuentaDeLuis.depositoEnCuenta(100);
-console.log("El nuevo saldo es" + saldo);
-// Retiro de la cuenta corrienta
-cuentaDeLuis.retiroEnCuenta(1000);
-console.log("El nuevo saldo es" + saldo);
-// Nuevo deposito a la cuenta corrienta
-cuentaDeLuis.depositoEnCuenta(-1000);
-console.log("El saldo actual es" + saldo);
+// /* Ejemplos para crear isntancias de las propiedades de la clase Cuenta corriente*/
+// // Deposito a la cuenta corrienta
+// saldo = cuentaDeLuis.depositoEnCuenta(100);
+// console.log("El nuevo saldo es" + saldo);
+// // Retiro de la cuenta corrienta
+// cuentaDeLuis.retiroEnCuenta(1000);
+// console.log("El nuevo saldo es" + saldo);
+// // Nuevo deposito a la cuenta corrienta
+// cuentaDeLuis.depositoEnCuenta(-1000);
+// console.log("El saldo actual es" + saldo);
+
+
+// Nuevo código
+
+/*Importación de clases*/
+import {Cliente} from './cliente.js'
+import {CuentaCorriente} from './cuentaCorriente.js';
+
+const cliente = new Cliente();
+cliente.nombreCliente = 'Leonardo';
+cliente.dniCliente = '13804050';
+cliente.rutCliente = '123224';
+
+const cuentaDeLeonardo = new CuentaCorriente();
+cuentaDeLeonardo.numero = '1';
+cuentaDeLeonardo.agencia = '001';
+cuentaDeLeonardo.cliente = cliente;
+
+//cuentaDeLeonardo.#saldo = 10;
+let saldo = cuentaDeLeonardo.verSaldo(); 
+
+saldo = cuentaDeLeonardo.depositoEnCuenta(150);
+console.log('El Saldo actual (CuentaLeonardo) '+saldo);
+
+const cliente2 = new Cliente();
+cliente2.nombreCliente = 'Maria';
+cliente2.dniCliente = '16979808';
+cliente2.rutCliente = '8989';
+
+
+const cuentaDeMaria = new CuentaCorriente();
+cuentaDeMaria.numero = '2';
+cuentaDeMaria.agencia = '002';
+cuentaDeMaria.cliente = cliente2;
+
+console.log (cuentaDeMaria.cliente);
+
+cuentaDeMaria.cliente = 0;
+
+console.log (cuentaDeMaria.cliente);
+
+let parametroValor = 100;
+/*
+console.log('Parámetro Valor',parametroValor);
+cuentaDeLeonardo.transferirParaCuenta(parametroValor,cuentaDeMaria);
+console.log('Parámetro Valor',parametroValor);
+
+const saldoMaria = cuentaDeMaria.verSaldo();
+
+console.log('Cuenta de Maria',cuentaDeMaria);
+
+
+
+console.log('El Saldo actual (cuentaMaria) '+saldoMaria);
+
+const saldoLeonardo = cuentaDeLeonardo.verSaldo();
+console.log('El Saldo actual (cuentaLeonardo) '+saldoLeonardo);
+*/
